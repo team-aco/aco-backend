bb2b6b8e0848ef074d7b83b35f80e6a48753537b
smallThinking <wnsdnek778@gmail.com>
2019-10-10 12:18:41 +0900

    setup prisma

--- src/routes/test/index.js
+++ src/routes/test/index.js
@@ -4,5 +4,6 @@ import * as testCtrl from 'routes/test/test.ctrl';
 const test = new Router();
 
 test.get('/hello', testCtrl.helloWorld);
+test.get('/prisma', testCtrl.getUser);
 
 export default test;
--- src/routes/test/test.ctrl.js
+++ src/routes/test/test.ctrl.js
@@ -1,3 +1,11 @@
+import { prisma } from 'generated/prisma-client';
+
 export const helloWorld = (ctx, next) => {
-    ctx.body = 'hello world';
-}
+  ctx.body = 'hello world';
+};
+
+export const getUser = async (ctx, next) => {
+  console.log(prisma);
+
+  ctx.body = await prisma.users();
+};
